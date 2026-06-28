a463d2693cc35ce4517980c1452e2a7cecb9a3ef
Tarunj05 <jaiswalt54@gmail.com>
2026-06-28 16:08:35 +0530

    Time: 0 ms (100%), Space: 13.9 MB (93.03%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     
-    void backtrack( int i ,vector<int>& nums , int target , vector<int>& templist , vector<vector<int>>& list){
-        if( i == nums.size()){
-            if( target == 0) list.push_back( templist );
-            return;
-        }
+    void backtrack( int start ,vector<int>& nums , int target , vector<int>& templist , vector<vector<int>>& list){
         if( target == 0){
             list.push_back( templist );
             return;
         }
 
-        //choose 
-        if( nums[i] <= target ){
-            templist.push_back( nums[i]);
-            //explore all 
-            // *** you can choose ith element again ***
-            backtrack( i ,  nums , target - nums[i] , templist , list );
-            //undo
-            templist.pop_back();
+        for( int i = start ; i<nums.size() ; i++){
+            if( nums[i] <= target){
+                templist.push_back( nums[i] );
+                backtrack( i , nums , target - nums[i] , templist , list);
+                templist.pop_back();
+            }
         }
-        //don't choose , just explore next
-        backtrack( i+1 , nums ,target , templist ,list);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
