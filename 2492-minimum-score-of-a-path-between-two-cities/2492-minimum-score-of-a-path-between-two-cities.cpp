c705e72e39837a4935b5ede045ed0cbbacedcc7d
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-07-04 12:43:34 +0530

    Time: 61 ms (56.74%), Space: 132.8 MB (67.96%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-
-void dfs( int u , int& ans ,vector<bool>& vis , vector<vector<pair<int,int>>>& adjLs){
-        vis[u] = 1;
-
-        for( auto [v,w] : adjLs[u] ){
-            
-            // update the ans for every neighbour
-            // as you can get to the nth city by crossing through this edge
-            // even when you have to cross some edges twice
-            // because they are on same connected component
-
-            ans = min ( ans , w );
-
-            if( !vis[v] ){
-                dfs( v , ans , vis , adjLs );
-            }
-        }
-
-    }
-
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adjLs ( n + 1 );
-        vector<bool> vis ( n + 1 , 0 );
+        
+
+        vector<vector<pair<int,int>>> adjLs( n+1 );
 
         for( auto& e : roads ){
             int u = e[0] , v = e[1] , w = e[2];
 
-            adjLs[u].push_back({v , w});
-            adjLs[v].push_back({u , w});// undirected graph
+            adjLs[u].push_back({v,w});
+            adjLs[v].push_back({u,w});
         }
 
-        // now we do a dfs traversal as it traverses the connected component and track the min weight edge in it
-        int ans = 1e5 ;
+        queue<int> q;
+        vector<bool> vis( n+1 , 0 );
 
-        dfs( 1 , ans , vis , adjLs );
+        int ans = 1e5;
 
-        return ans ;
+        // put the source node in the queue
+        q.push(1);
+        vis[1] = 1;
+        
+        while( !q.empty() ){
+            int curr = q.front();
+            q.pop();
+
+
+
+            // push the unvisited Neighbour
+
+            for( auto& [ v , w ] : adjLs[curr]){
+                // update the ans with the min dist in component connected to 1
+                ans = min( ans , w );                
+                
+                if( !vis[v] ){
+                    q.push(v);
+                    vis[v] = 1;
+                }
+            }
+
+        }
+        return ans;
     }
 };
