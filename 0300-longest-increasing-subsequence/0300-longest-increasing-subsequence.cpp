cb06c4331e7ab20f044343b326f9278627fbf685
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-07-16 13:40:33 +0530

    Time: 153 ms (37.71%), Space: 14.5 MB (40.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,39 +2,22 @@ class Solution {
 public:
 
 
-    int solve( int i , int prev , vector<int>&nums , vector<vector<int>>& dp){
-        // this is a function the gives the maximun length of increasing subsequence 
-        // starting from index i and previously chosen element's index as prev
-
-
-        // so according to the question we need solve( 0 , -1 ) ,  here -1 tell that no element has been chosen previously
-
-        // base case
-        if( i == nums.size()){
-            // we are out of bounds, so no subSeq. can be made now
-            return 0;
-        }
-
-        if( dp[i][prev+1] != -1 ) return dp[i][prev+1] ;
-
-        //choice 1 : Take the current element
-
-        int take = 0;
-
-        if( prev == -1 || nums[i] > nums[prev] ){
-            take = 1 + solve ( i+1 , i , nums , dp );
-        }
-
-        // choice 2 : skip the current element
-
-        int skip = solve( i+1 , prev , nums , dp );
-
-        return dp[i][prev+1] = max( take , skip );
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n =  nums.size();
+        int n = nums.size();
+        vector<int> next(n+1 , 0) , curr(n+1,0);
 
-        vector<vector<int>> dp( n , vector<int>( n + 1 , -1 ));
-        return solve( 0 , -1 , nums , dp );
+        for( int i = n-1 ; i>=0 ; i--){
+            for( int prev = i-1 ; prev>=-1 ; prev--){
+                    //not take
+                    int len =  0 + next[prev+1];
+                    //take
+                    if( prev == -1 || nums[i] > nums[prev] ){
+                        len = max( len , 1 + next[i+1]);
+                    }
+                    curr[prev+1] = len;
+            }
+            next = curr;
+        }
+        return curr[-1+1];
     }
 };
