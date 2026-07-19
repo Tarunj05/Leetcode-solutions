071df79f592e63d7d6843f73b8748b56c8158485
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-07-19 19:45:46 +0530

    Time: 1847 ms (15.98%), Space: 640.4 MB (17.02%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,51 +1,36 @@
 class Solution {
 public:
 
+    bool isPred( string prevWord , string currWord ){
+        int M = prevWord.size() , N = currWord.size();
+        if( N - M != 1)return 0;
 
-    static bool cmp( string s1 , string s2){
-        return s1.length() < s2.length();
-    }
-
-    bool isPred( string& a , string& b){
-        int M = a.length() ,N = b.length(); 
-        if(N - M != 1) return 0;        
-        int i = 0 , j = 0 ; 
+        int i = 0 , j = 0;
         while( i<M && j<N){
-            if(a[i] == b[j]) i++;
+            if( prevWord[i] == currWord[j])i++;
             j++;
         }
         return i == M;
     }
 
     int longestStrChain(vector<string>& words) {
+
+        sort( words.begin() , words.end() , [](auto& a , auto& b){
+            return a.length() < b.length();
+        });
         int n = words.size();
-        sort( words.begin() , words.end() , cmp );
 
-        // changing parameters : i , prev , dp state : dp[n+1][n+1];
+        vector<int> dp( n , 1);
+        int maxi = 1;
 
-        vector<vector<int>> dp( n+1 , vector<int> ( n + 1 ,0 ));
-
-        // base case , when i == n, dp[i][prev+1] = 0;
-        // dp table is already initialized with 0 , so no need to specify it
-
-        // changing parameters loop
-
-        for( int i = n-1 ; i>=0 ; i--){
-            for( int prev = -1  ; prev < i ; prev++){
-                // not take
-                int len = dp[i+1][prev+1];
-
-                if( prev == -1 || isPred( words[prev] , words[i])){
-                    len = max( len , 1 + dp[i+1][i+1]);
+        for(int i = 0 ; i<n ; i++){
+            for( int prev = 0 ; prev <  i ; prev++){
+                if( isPred( words[prev] , words[i])){
+                    dp[i]= max( dp[i] , dp[prev] + 1 );
+                    maxi = max( dp[i] , maxi);
                 }
-                dp[i][prev+1] = len;
             }
         }
-
-        // 
-
-
-
-        return dp[0][0];
+        return maxi;
     }
 };
