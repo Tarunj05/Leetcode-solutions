2857b7f4f3488befd507c580151421c5306087f9
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-07-20 20:14:16 +0530

    Time: 0 ms (100%), Space: 18.1 MB (66.35%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        if( k == 0 )return grid;
-        int m = grid.size() ,  n = grid[0].size();
+        int m = grid.size() , n = grid[0].size();
 
-        vector<vector<int>> result ( m , vector<int> ( n , 0 ));
+        if( k == 0 || (m == 1 && n==1)) return grid;
 
-        while( k ){
-            for( int i = 0 ; i < m ; ++i ){
-                for( int j = 0 ; j <n ; ++j ){
-                    if( j == n-1 ){
-                        if( i == m-1) result[0][0] = grid[i][j];
-                        else result[i+1][0] = grid[i][j];
-                    }
-                    else result[i][j+1] = grid[i][j];
+        // assume it as a flattened array
+        // so 1d index =  i*n + j
 
-                }
+        // modulo the k by total elements
+        // becase shifting it by total elements we get the same array
+        int total = m*n;
+        k %= total;
+
+        // now we traverse the given grid one by one and put elements to the shifted array
+
+        vector<vector<int>> result ( m , vector<int> (n  , 0));
+        for( int i=0 ; i<m ; ++i){
+            for( int j=0 ; j<n ; ++j){
+                int oldIdx = i*n + j;
+                int newIdx = (oldIdx + k) % total;
+                int newRow = newIdx / n;
+                int newCol = newIdx % n;
+
+                result[newRow][newCol] = grid[i][j];
             }
-            grid = result;
-            k--;
         }
         return result;
     }
