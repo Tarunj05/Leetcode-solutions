8402e79df55e87db1ac393603c4624ada59d5706
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-08-01 16:37:31 +0530

    Time: 0 ms (100%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,17 +3,26 @@ public:
 
     int dp[22][22];
 
-    int maxDiff( int i , int j , vector<int>& nums){
-        if( i==j) return nums[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        int ans = nums[i] - maxDiff(i+1, j , nums);
-        ans = max( ans , nums[j] - maxDiff(i,j-1,nums));
+    int solve( int i , int j , vector<int>& nums){
+        // gives the max score of p1
+        if( i>j) return 0;
 
-        return dp[i][j]=ans;
+        if( dp[i][j] != -1) return dp[i][j];
+
+        // choice 1 : take i
+        int score = nums[i] + min( solve(i+2,j,nums) , solve(i+1,j-1,nums) );
+        //choice 2 : take j
+        score = max( score , nums[j] + min( solve(i+1,j-1,nums) , solve(i,j-2,nums)));
+
+        return dp[i][j] = score;
     }
 
     bool predictTheWinner(vector<int>& nums) {
-        memset( dp , -1 , sizeof(dp));
-        return maxDiff( 0 , nums.size()-1 , nums) >= 0;
+        memset(dp,-1,sizeof( dp ));
+        int n = nums.size();
+        int p1 = solve( 0 , n-1 , nums);
+        int total = accumulate( nums.begin() , nums.end() , 0);
+        int p2 = total - p1;
+        return p1 >= p2;
     }
 };
