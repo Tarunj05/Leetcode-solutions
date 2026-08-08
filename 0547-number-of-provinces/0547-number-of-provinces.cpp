44f64ace0ad8338ccf99a695c187aabff9f11eec
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-08-08 14:39:00 +0530

    Time: 0 ms (100%), Space: 19.5 MB (71.72%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,40 +1,52 @@
-class Solution {
+class disjointSet{
 public:
-
-    void bfs( int vertex , vector<int>& vis , vector<vector<int>>& adj ){
-        queue<int> q;
-        q.push(vertex);
-        vis[vertex]=1;
-        while(!q.empty()){
-            int curr = q.front();q.pop();
-            for( int child : adj[curr]){
-                if( !vis[child]){
-                    q.push(child);
-                    vis[child]=1;
-                }
-            }
+    vector<int> parent , rank , size;
+    disjointSet( int n ){
+        parent.resize(n+1);
+        for( int i=0 ; i<=n ; i++){
+            parent[i] = i;
+        }
+        rank.resize( n+1 , 0);
+        size.resize( n+1 , 1);
+    }
+    int findUPar( int x){
+        if( parent[x] == x) return x;
+        return parent[x] = findUPar(parent[x]);
+    }
+    void unionByRank( int u , int v){
+        int ulp_u = findUPar(u) , ulp_v = findUPar(v);
+        if( ulp_u == ulp_v) return;
+        if( rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        }else if( rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }else{
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
         }
     }
+};
 
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adj(n);
+        disjointSet ds(n);
+
         for( int i=0 ; i<n ; i++){
-            for( int j=0 ; j<n ; j++){
+            for( int j =0 ; j<n ; j++){
                 if( i==j)continue;
-                if( isConnected[i][j] ){
-                    adj[i].push_back(j);
+                if( isConnected[i][j]){
+                    ds.unionByRank(i,j);
                 }
             }
         }
-        vector<int> vis( n , 0);
-        int  provinces = 0;
+        int province = 0;
         for( int i=0 ; i<n ; i++){
-            if(!vis[i]){
-                provinces++;
-                bfs( i , vis, adj);
+            if( ds.findUPar(i) == i){
+                province++;
             }
         }
-        return provinces;
+        return province;
     }
 };
