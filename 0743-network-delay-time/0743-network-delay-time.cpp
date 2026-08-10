b4ebde615e37e0dddd090250fdfd1c814a10f9df
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-08-10 14:30:14 +0530

    Time: 95 ms (22.18%), Space: 48.1 MB (17.68%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,32 +1,32 @@
+
 class Solution {
 public:
-
+    using p = pair<int,int>;
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>> adj(n+1);
-        for( auto time : times){
-            int u = time[0] , v = time[1] , wt = time[2];
+        for( auto it : times ){
+            int u = it[0] , v = it[1] , wt = it[2];
             adj[u].push_back({v,wt});
         }
-        priority_queue<pair<int,int>> pq;
-        const int INF = 1e9;
-        vector<int> dis( n+1 , INF );
+        vector<int> dis( n+1 , 1e9);
+        // push the source in the priortity queue
+        priority_queue<p , vector<p> , greater<p>> pq;//min Heap , {dis , node};
         pq.push({0,k});
-        dis[k]=0;
-
+        dis[k] = 0;
         while(!pq.empty()){
-            int u = pq.top().second; pq.pop();
-            for( auto [v , wt] : adj[u]){
-                if( dis[u] + wt < dis[v]){
+            int u = pq.top().second;
+            pq.pop();
+            for( auto [v,wt] : adj[u]){
+                if(dis[u]+wt < dis[v]){
                     dis[v] = dis[u] + wt;
-                    pq.push({dis[v] ,v});
+                    pq.push({dis[v],v});
                 }
             }
         }
-        int ans = 0;
-        for( int i=1 ; i<=n ; ++i){
+        int ans = 0 ;
+        for(int i = 1 ; i<=n ; ++i){
             ans = max( ans , dis[i]);
         }
-
-        return ans==INF ? -1 : ans;
+        return ans == 1e9 ? -1 :ans;
     }
 };
