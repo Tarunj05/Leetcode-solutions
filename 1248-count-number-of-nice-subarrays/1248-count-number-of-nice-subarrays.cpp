97b5737dfdf6c87ce4d898b70c19aa9ebc5f0456
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-08-16 03:25:06 +0530

    Time: 3 ms (89.14%), Space: 71.4 MB (70.4%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
+
+    int subarrays( int k , vector<int>& nums ){
+        if(k<0) return 0;
         int n = nums.size();
-        unordered_map<int,int> seenCount;
-        seenCount[0] = 1;
-        int currPrefixSum = 0;
-        int count = 0;
-        for( int i=0 ; i<n ; i++){
-            if( nums[i] % 2 != 0){
-                currPrefixSum ++;
+        int l=0;
+        int subarraySum = 0;
+        int subarrayCount = 0;
+        for( int r=0 ; r<n ; r++){
+            //include right
+            if( nums[r] % 2 != 0)
+                subarraySum += 1;
+            // shrink while invalid
+            while( subarraySum > k ){
+                if (nums[l] % 2 != 0) subarraySum -= 1;
+                l++;
             }
-            count += seenCount[ currPrefixSum - k ];
-            seenCount[ currPrefixSum ]++;
+            //here the window is valid
+            //so add the no of subarrays with sum <=k ending at index r , i.e. = r-l+1
+            subarrayCount += r-l+1;
         }
-        return count;        
+        return subarrayCount;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return subarrays( k , nums ) - subarrays( k-1 , nums );
     }
 };
