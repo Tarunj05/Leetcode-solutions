44f526df08d569a9df0d9d025b78790225d1ed8a
Tarun Jaiswal <jaiswalt54@gmail.com>
2026-08-30 21:59:11 +0530

    Time: 0 ms (100%), Space: 15.5 MB (88.92%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,43 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if( !head || !head -> next || !head-> next -> next) return head;
-
-        ListNode* temp = head;
-        ListNode* odd = new ListNode( -1 );
-        ListNode* even = new ListNode( -1 );
-        ListNode* oPtr = odd;
-        ListNode* ePtr = even;
-
-        bool isOdd = 1;
+        if( !head || !head -> next || !head -> next -> next ) return head;
+        ListNode* temp = head -> next -> next;
+        ListNode* oddEnd = head;
+        ListNode* evenStart = head -> next;
+        ListNode* evenEnd = head -> next;
 
         while( temp ){
-            if( isOdd ){
-                oPtr -> next = new ListNode( temp->val);
-                oPtr = oPtr -> next;
-            }else{
-                ePtr -> next = new ListNode( temp-> val);
-                ePtr = ePtr -> next;
-            }
-            temp = temp -> next;
-            isOdd = !isOdd;
+            ListNode* front = temp -> next;
+
+            oddEnd -> next = temp;
+            temp -> next = evenStart;
+            evenEnd -> next = front;
+
+            oddEnd = temp;
+            evenEnd = front;
+            if( front ) temp = front -> next;
+            else temp = nullptr;
         }
-
-        odd = odd -> next;
-        even = even -> next;
-
-        //join odd and even
-        oPtr -> next = even;
-        return odd;
+        return head;
     }
 };
